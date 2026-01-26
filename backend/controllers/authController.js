9e17eb70b9da10b8f063996c736ae3da044bcb41
Tulsi8645 <85345889+Tulsi8645@users.noreply.github.com>
2026-01-26 20:05:55 +0545

    token will not expire

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -126,8 +126,7 @@ const verifyGoogleTokenMobile = async (req, res) => {
     // Generate JWT token
     const token = jwt.sign(
       { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      process.env.JWT_SECRET
     );
 
     res.json({
